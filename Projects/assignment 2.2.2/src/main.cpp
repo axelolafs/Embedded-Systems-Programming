43e25b52d1eb8ab87834d151e7193537a8a68684
Axel Ólafsson <axelola18@ru.is>
2021-09-09 16:08:07 +0000

    ass 2.2

---
+++ Projects/assignment 2.2.2/src/main.cpp
@@ -0,0 +1,57 @@
+/**
+ * @file main.c
+ * @version 4.1.0
+ *
+ * @section License
+ * Copyright (C) 2015-2016, Erik Moqvist
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * This file is part of the Simba project.
+ */
+
+#include "simba.h"
+#include "digital_out.h"
+
+static THRD_STACK(blink_task_stack, 256);
+
+void* led_blink_task(void* arg_p){
+    Digital_out LED(5);
+    LED.init();
+    LED.set_hi();
+
+    while(1){
+        /* Wait half a second. */
+        thrd_sleep_us(500000);
+
+        /* Toggle the LED on/off. */
+        LED.toggle();
+    }
+}
+
+
+int main()
+{
+    /* Start the system. */
+    sys_start();
+
+    /* Spawn led blink thread */
+    thrd_spawn(led_blink_task,
+                NULL,
+                0,
+                blink_task_stack,
+                sizeof(blink_task_stack));
+
+    while(1){
+        thrd_sleep_us(1000);
+    }
+    return (0);
+}
---
+++ assignment 2.2/main.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file main.c
+ * @version 4.1.0
+ *
+ * @section License
+ * Copyright (C) 2015-2016, Erik Moqvist
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * This file is part of the Simba project.
+ */
+
+#include "simba.h"
+#include "digital_out.h"
+
+int main()
+{
+    // struct pin_driver_t led;
+    Digital_out LED(5);
+
+    /* Start the system. */
+    sys_start();
+
+    /* Initialize the LED pin as output and set its value to 1. */
+    LED.init();
+    LED.set_hi();
+
+    while (1) {
+        /* Wait half a second. */
+        thrd_sleep_us(500000);
+
+        /* Toggle the LED on/off. */
+        LED.toggle();
+    }
+
+    return (0);
+}
