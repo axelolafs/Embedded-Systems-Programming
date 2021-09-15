b6379b4f58a9ed67c2ef9f18362d4a78883f2858
Axel Ólafsson <axelola18@ru.is>
2021-09-15 02:11:52 +0000

    Part 3

---
+++ Projects/object behavioral state pattern/src/main.cpp
@@ -0,0 +1,136 @@
+#include <Arduino.h>
+
+class Context;
+
+class State {
+  /**
+   * @var Context
+   */
+ protected:
+  Context *context_;
+
+ public:
+  virtual ~State() {
+  }
+
+  void set_context(Context *context) {
+    this->context_ = context;
+  }
+
+  virtual void HandleStop() = 0;
+  virtual void HandleGo() = 0;
+};
+
+/**
+ * The Context defines the interface of interest to clients. It also maintains a
+ * reference to an instance of a State subclass, which represents the current
+ * state of the Context.
+ */
+class Context {
+  /**
+   * @var State A reference to the current state of the Context.
+   */
+ private:
+  State *state_;
+
+ public:
+  Context(State *state) : state_(nullptr) {
+    this->TransitionTo(state);
+  }
+  ~Context() {
+    delete state_;
+  }
+  /**
+   * The Context allows changing the State object at runtime.
+   */
+  void TransitionTo(State *state) {
+    if (this->state_ != nullptr)
+      delete this->state_;
+    this->state_ = state;
+    this->state_->set_context(this);
+  }
+  /**
+   * The Context delegates part of its behavior to the current State object.
+   */
+  void Stop() {
+    this->state_->HandleStop();
+  }
+  void Go() {
+    this->state_->HandleGo();
+  }
+};
+
+/**
+ * Concrete States implement various behaviors, associated with a state of the
+ * Context.
+ */
+
+class Red : public State {
+ public:
+  void HandleStop() override{
+    Serial.println("Red -> Red");
+  }
+  
+  void HandleGo() override;
+  
+};
+
+class Green : public State {
+ public:
+  void HandleStop() override;
+  void HandleGo() override {
+    Serial.println("Green -> Green");
+  }
+};
+
+class Yellow : public State {
+ public:
+  void HandleStop() override {
+    Serial.println("Yellow -> Yellow");
+  }
+  void HandleGo() override {
+    Serial.println("Yellow -> Yellow");
+  }
+};
+
+void Red::HandleGo() {
+  Serial.println("Red -> Green");
+  this->context_->TransitionTo(new Green);
+}
+
+void Green::HandleStop() {
+  Serial.println("Green -> Yellow");
+  this->context_->TransitionTo(new Yellow);
+}
+
+
+/**
+ * The client code.
+ */
+void streetlights() {
+  int command;
+  Context *context = new Context(new Red);
+  while(1){
+    if (Serial.available() > 0){
+      command = Serial.read();
+      switch (command){
+        case 'g':
+          context->Go();
+          break;
+        case 's':
+          context->Stop();
+          break;
+      }
+    }
+  }
+  delete context;
+}
+
+
+int main() {
+  init();
+  Serial.begin(9600);
+
+  streetlights();
+  return 0;
+}
