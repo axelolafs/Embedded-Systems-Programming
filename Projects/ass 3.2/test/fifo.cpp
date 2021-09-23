a6c3d7842d3edb39ede4eb314866892eb986e913
Axel Ólafsson <axelola18@ru.is>
2021-09-23 18:24:22 +0000

    part 1, test cases

---
+++ Projects/ass 3.2/test/fifo.cpp
@@ -0,0 +1,25 @@
+#include "fifo.h"
+
+Fifo::Fifo(){
+
+}
+
+int Fifo::get(){
+    return 0;
+}
+
+void Fifo::put(int item){
+
+}
+
+bool is_empty(){
+    return false;
+}
+
+bool is_full(){
+    return false;
+}
+
+void reset(){
+
+}
---
+++ Projects/ass 3.2/test/test_main.cpp
@@ -0,0 +1,103 @@
+
+#include <avr/delay.h>
+#include <unity.h>
+
+#include <fifo.h>
+
+void test_reset(void)
+{
+    // 1 Setup
+    Fifo f;
+    f.put(1);
+    f.put(2);
+    f.put(3);
+    f.reset();
+
+    // validate empty
+    TEST_ASSERT_TRUE(f.is_empty());
+}
+
+void test_normal_flow(void)
+{
+    // 1 Setup
+    Fifo f;
+    f.put(1);
+    f.put(2);
+    f.get();
+    f.put(3);
+
+    // 2-3 Execute and validate
+    TEST_ASSERT_EQUAL(2, f.get());
+    TEST_ASSERT_EQUAL(3, f.get());
+
+    // 4 Cleanup
+}
+
+void test_underflow(void)
+
+{
+    // 1 Setup
+    Fifo f;
+    f.put(1);
+    f.put(2);
+    f.get();
+    f.get();
+
+    // 2-3 Execute and validate
+    TEST_ASSERT_TRUE(f.is_empty());
+
+    // 4 Cleanup
+}
+
+void test_overflow(void){
+
+    // 1 Setup
+    Fifo f;
+    f.put(1);
+    f.get();
+    f.put(1);
+    f.put(1);
+    f.put(1);
+    f.put(1);
+    f.put(1);
+
+    TEST_ASSERT_TRUE(f.is_full());
+
+}
+
+void test_overwrite(void){
+
+    // 1 Setup
+    Fifo f;
+    // Put 5 values in buffer
+    f.put(2);
+    f.put(1);
+    f.put(1);
+    f.put(1);
+    f.put(1);
+
+    // Put 6th value, so the buffer should be overwritten
+    f.put(1);
+
+    TEST_ASSERT_EQUAL(f.get(), 1);
+
+
+}
+
+int main()
+{
+    // NOTE!!! Wait for >2 secs
+    // if board doesn't support software reset via Serial.DTR/RTS
+    _delay_ms(2000);
+
+    UNITY_BEGIN(); // IMPORTANT LINE!
+
+    RUN_TEST(test_reset);
+    RUN_TEST(test_normal_flow);
+    RUN_TEST(test_underflow);
+    // Add more unit tests here
+
+    UNITY_END(); // stop unit testing
+}
+
+
