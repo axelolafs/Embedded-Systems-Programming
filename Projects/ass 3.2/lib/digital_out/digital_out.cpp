f84d0018b4e86b4001ccafc914ced0be4cc6dd81
Axel Ólafsson <axelola18@ru.is>
2021-09-23 20:17:26 +0000

    part 2, complete

---
+++ Projects/ass 3.2/lib/digital_out/digital_out.cpp
@@ -0,0 +1,27 @@
+#include "digital_out.h"
+#include <avr/io.h>
+
+Digital_out::Digital_out(int pinNumber)
+{
+    pinMask = 1 << pinNumber;
+}
+
+void Digital_out::init()
+{
+    DDRB |= pinMask;
+}
+
+void Digital_out::set_hi()
+{
+    PORTB |= pinMask;
+}
+
+void Digital_out::set_lo()
+{
+    PORTB &= ~pinMask;
+}
+
+void Digital_out::toggle()
+{
+    PORTB ^= pinMask;
+}
--- Projects/ass 3.2/src/main.cpp
+++ Projects/ass 3.2/src/main.cpp
@@ -1,3 +1,49 @@
+#include <Arduino.h>
+#include "fifo.h"
+#include "digital_out.h"
+
+void timerIntSecond();
+Fifo fifo;
+Digital_out LED(5);
+
+
 int main(){
+  init();
+  Serial.begin(9600);
+  int serialData;
+  timerIntSecond();
+
+  while(1){
+    if(fifo.is_full()){
+      LED.set_hi();
+    }
+    else{
+      LED.set_lo();
+      if (Serial.available() > 0) {
+        serialData = Serial.read();
+        fifo.put(serialData);
+      }
+    }
+  }
   return 0;
+}
+
+void timerIntSecond(){
+  TCCR1B = 0;
+  TCCR1A = 0;
+  TIMSK1 = 0;
+  // prescaler to 1024 and CTC mode
+  TCCR1B |= (1 << WGM12) | (1 << CS12) | (1 << CS10);
+  
+  // Compare on a second interval
+  OCR1A = (16000000/1024) - 1;
+  // enable interrupt for compare register 1 A
+  TIMSK1 |= (1 << OCIE1A);
+  sei();
+}
+
+ISR(TIMER1_COMPA_vect){
+  if (!fifo.is_empty()){
+    fifo.get();
+  }
 }
